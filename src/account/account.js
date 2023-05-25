c3db97035995762af9f86aed8e261ca1c9a950c8
tats1820 <1003648568@u.icesi.edu.co>
2023-05-25 17:23:28 -0500

    instalación firebase account

--- src/account/account.js
+++ src/account/account.js
@@ -1,19 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  });
 
+
 // Configura tu proyecto de Firebase
-var firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_DOMINIO.firebaseapp.com",
-    projectId: "TU_PROJECT_ID",
-    storageBucket: "TU_BUCKET.appspot.com",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
+  const firebaseConfig = {
+    apiKey: "AIzaSyAq2LKJ8F2eG1OmGkcuVLMVbkidjq8lDMg",
+    authDomain: "perfil-999ac.firebaseapp.com",
+    projectId: "perfil-999ac",
+    storageBucket: "perfil-999ac.appspot.com",
+    messagingSenderId: "1015608397607",
+    appId: "1:1015608397607:web:1bac96c6d874be35302302"
   };
-  
-  // Inicializa Firebase
-  firebase.initializeApp(firebaseConfig);
   
   // Obtiene una referencia al formulario de inicio de sesión
   var loginForm = document.getElementById("loginForm");
@@ -42,4 +45,7 @@ var firebaseConfig = {
         // Aquí puedes mostrar un mensaje de error al usuario
       });
   });
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
   
