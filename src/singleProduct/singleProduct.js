c451b0eb0d419d3b0e9e76c78def2cbcde1e1686
Lachosk <nicolasorozcobarrios@gmail.com>
2023-06-11 13:02:08 -0500

    el error nada de nada

--- src/singleProduct/singleProduct.js
+++ src/singleProduct/singleProduct.js
@@ -1,39 +1,32 @@
-
 import {
   getProdcuts
 } from "../firebase.js";
 
 const link = window.location.search;
-/* const buscarPagina = new URLSearchParams(link); */
 const buscarPagina = new URLSearchParams(link);
 const singleProduct = buscarPagina.get("id").replace('"', "");
 const buttonComentario = document.getElementById("comentario-button");
 const botonCarrito = document.getElementById("añadir");
 
-/* const productoPorSeparado = productsList.find((object) => object.id == singleProduct) */
 traerProducto();
-/////
+
 async function traerProducto() {
-  let productos = await getProdcuts()
-  console.log(productos)
-  const productoPorSeparado = productos.find(
-    (data) => data.id == singleProduct
-  );
+  let productos = await getProdcuts();
+  console.log(productos);
+  const productoPorSeparado = productos.find((data) => data.id == singleProduct);
   pintar(productoPorSeparado);
 }
 
 function pintar(productoPorSeparado) {
-
   const productosLink = document.getElementById("productos");
   productosLink.innerHTML = "";
   productosLink.innerHTML =
-    /* productosLink.innerHTML +=  */
     `<section class="card_list">
-      <figure class= "card_figure"><img class= "card_img" src="${productoPorSeparado.imagenProducto}"></figure>
+      <figure class="card_figure"><img class="card_img" src="${productoPorSeparado.imagenProducto}"></figure>
       <article class="card_article">
           <h2 class="name_detail">${productoPorSeparado.item}</h2>
-          <p class='descri_detail'>${productoPorSeparado.sobreModelo}</p>
-          <h5 class="price_detail"> ${productoPorSeparado.precio}</h5>
+          <p class="descri_detail">${productoPorSeparado.sobreModelo}</p>
+          <h5 class="price_detail">${productoPorSeparado.precio}</h5>
           <p class="collect_detail">Collection: ${productoPorSeparado.tipo}</p>
           
           <button id="añadir" class="button is-black button_añadir">Añadir</button>
@@ -42,47 +35,34 @@ function pintar(productoPorSeparado) {
           <input type="text" id="comentario-input"></input>
           <button id="comentario-button" onclick="addTocomentarioArray()">Enviar comentario</button>
           <div id="error-message" style="display: none; color: red;">Por favor ingrese un texto</div>
-          <div id = "comentario-list">
-          </div>
-          <div id = "comentarioRobot-list"></div>
+          <div id="comentario-list"></div>
+          <div id="comentarioRobot-list"></div>
           <div id="message-container" style="display: block;">No hay comentarios</div>
-      
-          </article>
+      </article>
   </section>`;
-
-
 }
 
-
 function addTocomentarioArray() {
-  // Obtener el texto de entrada del usuario
   const inputText = document.getElementById("comentario-input").value.trim();
-
-  // Limpiar el campo de entrada del usuario
   document.getElementById("comentario-input").value = "";
 
-  // Mostrar un mensaje de error si el usuario no ingresó nada
   if (inputText === "") {
     document.getElementById("error-message").style.display = "block";
     return;
   }
 
-  // Ocultar el mensaje de error
   document.getElementById("error-message").style.display = "none";
 
-  // Obtener el ID del producto actual y su lista de comentarios
-  const currentId = singleProduct;
-  const currentProduct = productsList.find((data) => data.id == currentId);
+  const currentProduct = productoPorSeparado;
   const comentarioArray = currentProduct.comentarioArray || [];
 
-  // Agregar el nuevo comentario a la lista de comentarios
   comentarioArray.push(inputText);
   document.getElementById("message-container").style.display = "none";
   currentProduct.comentarioArray = comentarioArray;
 
-  // Mostrar todos los comentarios, incluido el nuevo
   const outputList = document.getElementById("comentario-list");
   outputList.innerHTML = "";
+
   comentarioArray.forEach((item, i) => {
     const li = document.createElement("li");
     li.classList.add("comentario-item");
@@ -117,35 +97,34 @@ function addTocomentarioArray() {
 }
 
 function deleteComment(index) {
-  let currentId = singleProduct;
-  let currentProduct = productsList.find((data) => data.id == currentId);
-  let comentarioArray = currentProduct.comentarioArray || [];
+  const currentProduct = productoPorSeparado;
+  const comentarioArray = currentProduct.comentarioArray || [];
   comentarioArray.splice(index, 1);
   currentProduct.comentarioArray = comentarioArray;
 
-  let outputList = document.getElementById("comentario-list");
+  const outputList = document.getElementById("comentario-list");
   outputList.innerHTML = "";
 
   comentarioArray.forEach((item, i) => {
-    let li = document.createElement("li");
+    const li = document.createElement("li");
     li.classList.add("comentario-item");
 
-    let imgProfile = document.createElement("img");
+    const imgProfile = document.createElement("img");
     imgProfile.setAttribute(
       "src",
       "https://www.daysoftheyear.com/wp-content/uploads/international-cat-day1-scaled.jpg"
     );
     imgProfile.classList.add("image-profile");
 
-    let nameProfile = document.createElement("p");
+    const nameProfile = document.createElement("p");
     nameProfile.innerHTML = "Lacho";
     nameProfile.classList.add("name-profile");
 
-    let content = document.createElement("p");
+    const content = document.createElement("p");
     content.innerHTML = item;
     content.classList.add("comentario-content");
 
-    let deleteButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
     deleteButton.innerHTML = "Eliminar";
     deleteButton.onclick = () => deleteComment(i);
     deleteButton.classList.add("delete-button");
@@ -163,38 +142,31 @@ function deleteComment(index) {
   }
 }
 
-
 let commentPreDiv = document.createElement("div");
 let commentList = "<ul>";
-productoPorSeparado.comments.forEach((comment) => {
-  if (productoPorSeparado.comments.length === 0) {} else {
-    document.getElementById("message-container").style.display = "none";
 
+productoPorSeparado.comentarioArray.forEach((comment) => {
+  if (productoPorSeparado.comentarioArray.length === 0) {
+    // Do nothing
+  } else {
+    document.getElementById("message-container").style.display = "none";
     commentList += `
-    <li class= "comentarioRobotList">
-    <img class="userRobotImg" src="${comment.profileUrl}" />
-      <p class= "userRobotName">${comment.name}</p>
-      <p class= "userRobotComment">${comment.comment}</p>
-    </li>
-  `;
+    <li class="comentarioRobotList">
+      <img class="userRobotImg" src="${comment.profileUrl}" />
+      <p class="userRobotName">${comment.name}</p>
+      <p class="userRobotComment">${comment.comment}</p>
+    </li>`;
   }
 });
+
 commentList += "</ul>";
 commentPreDiv.innerHTML = commentList;
 const comentarioRobot = document.getElementById("comentarioRobot-list");
 comentarioRobot.appendChild(commentPreDiv);
 
-
-
-
-// get the size selector element
 const sizeSelector = document.getElementById("size-selector");
 
-// add an event listener to the size selector
 sizeSelector.addEventListener("change", function () {
-  // get the selected value
   const selectedSize = sizeSelector.value;
-
-  // do something with the selected value
   console.log("Selected size: " + selectedSize);
 });
